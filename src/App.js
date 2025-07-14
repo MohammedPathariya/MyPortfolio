d221c8d998d100d8173f4de26ba7239fc4e44e42
MohammedPathariya <pathmohd123@gmail.com>
2025-07-14 18:40:56 -0400

    Modified the code to remove the About and Skill Sections imported but unused issue

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
+// import About from './components/About';
 import Projects from './components/Projects';
-import Skills from './components/Skills';
+// import Skills from './components/Skills';
 import TimelineTabs from './components/TimelineTabs';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
